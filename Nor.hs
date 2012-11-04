4c95151127ea5effe3d3a517f306ea246313ad27
hashem <hnasarat@gmail.com>
2012-11-03 22:48:57 -0400

    Add basic datatypes
    From work with JC and Matt:
    File - path, lines
    Hash - string
    Commit - has parent, hashes, id
    Repo - list (conceptually a set) of all commits
    World - Repo, hash-to-file dictionary, HEAD commit, commit count

--- Nor.hs
+++ Nor.hs
@@ -3,3 +3,23 @@ module Nor where
 commit :: [String] -> IO ()
 commit _ = putStrLn "commit"
 
+---------------------------------
+
+data File = File { path :: String -- Unix filepath: "/foo/bar/baz"
+                 , lines :: [String] -- Simple representation for now
+                 } deriving (Show)
+type Hash = String -- Cryptographic hash
+
+
+mkHashDict :: Hash -> Maybe File
+mkHashDict = \_ -> Nothing
+
+data Commit = Commit { parent :: Maybe Commit -- Initial commit has nothing
+                     , newHs :: [Hash] -- Hashes of files which changed
+                     , cid :: Int -- Unique identifier
+                     } deriving (Show)
+type Repo = [Commit]
+
+-- head commit, hash->file, head commit, commitCount
+type World = (Repo, Hash -> Maybe File, Maybe Commit, Int)
+
