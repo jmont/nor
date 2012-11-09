3b14d24dd1f0c6ac9d93371a86c3c325fb8ba64b
hashem <hnasarat@gmail.com>
2012-11-08 22:14:56 -0500

    Add more test cases

--- Diff.hs
+++ Diff.hs
@@ -1,11 +1,39 @@
 import Data.Algorithm.Diff
 
+a0 = ["a"]
+a1 = ["c", "b"]
+a2 = ["c", "c", "c"]
+
+da01 = getDiff a0 a1
+da02 = getDiff a0 a2
+
+b0 = ["a", "b", "c"]
+b1 = ["a", "b", "d"]
+b2 = ["a", "b", "e"]
+
+db01 = getDiff b0 b1
+db02 = getDiff b0 b2
+
+c0 = ["a", "b", "c"]
+c1 = ["b", "d"]
+c2 = ["b", "e"]
+
+dc01 = getDiff c0 c1
+dc02 = getDiff c0 c2
+
 f0 = ["cat", "dog"]
 f1 = ["cat", "me", "cat", "hello"]
 f2 = ["cat", "dog", "matt", "JC"]
 
-d01 = getDiff f0 f1
-d02 = getDiff f0 f2
+df01 = getDiff f0 f1
+df02 = getDiff f0 f2
+
+g0 = ["matt","me"]
+g1 = ["hey","me"]
+g2 = ["bob","matt","hello","me"]
+
+dg01 = getDiff g0 g1
+dg02 = getDiff g0 g2
 
 --d1s and d2s are diffs from a common ancestor
 combinediff :: Eq t => [(DI, t)] -> [(DI, t)] -> [t]
