a35bab7450afd6b86157681ac2d4c724b5046877
Matthew Russell <matthewjosephrussell@gmail.com>
2012-11-09 13:58:15 -0500

    Combinediff now returns the "merged" diff
    combinediff returns a new diff which represents the diff from the original
    file and a new file which is the result of a merge of the changes from
    the original diffs. Currenlty uses the "both" merge strategy.
    
    Added in helper function createFromDiff which returns a file representation
    of the diff (without any things which have been removed in the new file)

--- Diff.hs
+++ Diff.hs
@@ -36,18 +36,21 @@ dg01 = getDiff g0 g1
 dg02 = getDiff g0 g2
 
 --d1s and d2s are diffs from a common ancestor
-combinediff :: Eq t => [(DI, t)] -> [(DI, t)] -> [t]
-combinediff [] d2s = map snd d2s
-combinediff d1s [] = map snd d1s
-combinediff ((S, t1):d1s) (d2@(S, t2):d2s) =
+combinediff :: Eq t => [(DI, t)] -> [(DI, t)] -> [(DI,t)]
+combinediff [] d2s = d2s
+combinediff d1s [] = d1s
+combinediff (d1@(S, t1):d1s) (d2@(S, t2):d2s) =
     if t1 == t2
-        then t1:combinediff d1s d2s -- add same only once
-        else t1:combinediff d1s (d2:d2s)
-combinediff ((S, t1):d1s) d2s = t1:combinediff d1s d2s --added so add
-combinediff d1s ((S, t2):d2s) = t2:combinediff d1s d2s --added so add
+        then d1:combinediff d1s d2s -- add same only once
+        else d1:combinediff d1s (d2:d2s)
+combinediff (d1@(S, t1):d1s) d2s = d1:combinediff d1s d2s --added so add
+combinediff d1s (d2@(S, t2):d2s) = d2:combinediff d1s d2s --added so add
 combinediff ((bof1, t1):d1s) ((bof2, t2):d2s) =
     if t1 == t2
         then case (bof1, bof2) of
-            (B,B) -> t1:combinediff d1s d2s -- both kept so keep
-            _ -> combinediff d1s d2s -- >1 removed so remove
+            (B,B) -> (B,t1):combinediff d1s d2s -- both kept so keep
+            _ -> (F,t1):combinediff d1s d2s -- >at least one removed so remove
         else error "bad"
+
+createFromDiff :: [(DI,t)] -> [t]
+createFromDiff diffs = map snd (filter (\(x,_) -> x/= F) diffs)
