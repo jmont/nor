2766db96e08aea7d3f265baae7c5460b35f22c83
hashem <hnasarat@gmail.com>
2012-11-08 22:19:19 -0500

    Add expected combinediff output

--- Diff.hs
+++ Diff.hs
@@ -3,35 +3,35 @@ import Data.Algorithm.Diff
 a0 = ["a"]
 a1 = ["c", "b"]
 a2 = ["c", "c", "c"]
-
+--cbcc
 da01 = getDiff a0 a1
 da02 = getDiff a0 a2
 
 b0 = ["a", "b", "c"]
 b1 = ["a", "b", "d"]
 b2 = ["a", "b", "e"]
-
+--abde
 db01 = getDiff b0 b1
 db02 = getDiff b0 b2
 
 c0 = ["a", "b", "c"]
 c1 = ["b", "d"]
 c2 = ["b", "e"]
-
+--bde
 dc01 = getDiff c0 c1
 dc02 = getDiff c0 c2
 
 f0 = ["cat", "dog"]
 f1 = ["cat", "me", "cat", "hello"]
 f2 = ["cat", "dog", "matt", "JC"]
-
+--cat me cat hello matt jc
 df01 = getDiff f0 f1
 df02 = getDiff f0 f2
 
 g0 = ["matt","me"]
 g1 = ["hey","me"]
 g2 = ["bob","matt","hello","me"]
-
+--hey bob hello me
 dg01 = getDiff g0 g1
 dg02 = getDiff g0 g2
 
