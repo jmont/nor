f63ea5951ea77109542992c8a39871c087f8b206
Juan Carlos Montemayor Elosua <j.mont@me.com>
2012-11-23 17:05:12 -0500

    Formatting changes

--- Patch.hs
+++ Patch.hs
@@ -1,12 +1,11 @@
 module Patch where
-
 import Data.Algorithm.Diff
 
 type Edit = (DI,String)
 type Path = String
 
 data Patch = AtPath Path PatchAction
-             | Atomic [Patch] deriving (Show)
+           | Atomic [Patch] deriving (Show)
 
 data PatchAction = RemoveEmptyFile
                  | CreateEmptyFile
@@ -30,21 +29,19 @@ editsToPatch es p = Atomic $ map (AtPath p) (editsToChangeHunks es)
 
 editsToChangeHunks :: [Edit] -> [PatchAction]
 editsToChangeHunks es = eTCH es 0
-   where eTCH es lineNum =
-          let keeps = takeWhile eqB es
-              rest = dropWhile eqB es
-              changes = takeWhile neqB rest
-              rest'' = dropWhile neqB rest
-              deletes = map snd (filter eqF changes)
-              adds = map snd (filter neqF changes)
-              ch = ChangeHunk (lineNum + length keeps) deletes adds
-          in if (length adds + length deletes) == 0
-             then []
-             else ch : eTCH rest'' (offset ch + length adds - length deletes)
-         eqB = ((==) B . fst)
+   where eqB = ((==) B . fst)
          neqB = ((/=) B . fst)
          eqF = ((==) F . fst)
          neqF = ((/=) F . fst)
+         eTCH es lineNum =
+            let (keeps, rest) = span eqB es
+                (changes, rest'') = span neqB rest
+                deletes = map snd (filter eqF changes)
+                adds = map snd (filter neqF changes)
+                ch = ChangeHunk (lineNum + length keeps) deletes adds
+             in if (length adds + length deletes) == 0
+                then []
+                else ch : eTCH rest'' (offset ch + length adds - length deletes)
 
 mergePatches :: Patch -> Patch -> Patch
 mergePatches p1 p2 = error "Not written yet"
