aab44d4587e8ee68a4ecb3bbbab1a1d391ab8c70
hashem <hnasarat@gmail.com>
2012-11-25 11:53:16 -0500

    Initialize .nor and .nor/world when they're not there
    The two things don't exist when nor is first run. This implementation isn't
    the greatest -- you get the same error each time you run nor until you run it
    with the proper usage -- but it works. The errors each time stem from not
    creating the world until after a proper command has been passed to dispatch.

--- Main.hs
+++ Main.hs
@@ -1,14 +1,17 @@
 import Control.Monad
 import System.Environment
+import System.Directory
 import System.IO
 import Data.List
 import Data.Serialize
+import qualified Control.Exception as E
 import qualified Data.Set as Set
 import qualified Data.ByteString as S
 import qualified ObjectStore as O
 import qualified Nor as N
 
-worldPath = "./.nor/world"
+progDirPath = "./.nor"
+worldPath = progDirPath ++ "/world"
 
 saveWorld :: N.World -> IO ()
 saveWorld w = do
@@ -17,18 +20,24 @@ saveWorld w = do
     hClose handle
 
 getWorld' :: IO (Either String N.World)
-getWorld' = do
-    handle <- openFile worldPath ReadMode
-    encodedW <- S.hGetContents handle
-    hClose handle
-    return $ decode encodedW
+getWorld' = E.catch
+    (do handle <- openFile worldPath ReadMode
+        encodedW <- S.hGetContents handle
+        hClose handle
+        return $ decode encodedW)
+    (\(e) -> hPutStrLn stderr (show (e :: E.IOException)) >> (return $ Left ""))
+
+createProgDir :: IO ()
+createProgDir = E.catch
+    (createDirectory progDirPath)
+    (\(e) -> hPutStrLn stderr (show (e :: E.IOException)))
 
 getWorld :: IO (N.World)
 getWorld = do
     eitherW <- getWorld'
-    return $ case eitherW
-               of Left err -> N.init
-                  Right w -> w
+    case eitherW of
+        Left err -> createProgDir >> (return N.init)
+        Right w -> return w
 
 getFile :: String -> IO(N.File)
 getFile p = do
