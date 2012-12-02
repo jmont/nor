47dfb6014affd108d6bca0ee7e46784eb92b5f33
Matthew Russell <matthewjosephrussell@gmail.com>
2012-12-02 14:02:50 -0500

    Don't import Nor as qualified

--- Main.hs
+++ Main.hs
@@ -10,13 +10,13 @@ import qualified Data.Set as Set
 import qualified Data.ByteString as S
 import Data.Serialize
 import qualified ObjectStore as O
-import qualified Nor as N
+import Nor
 import qualified Control.Monad.State as State
 
-type World = (N.Core, N.Commit)
+type World = (Core, Commit)
 
 initWorld :: World
-initWorld = let core@(commitSet,os) = N.initCore
+initWorld = let core@(commitSet,os) = initCore
             in (core,head $ Set.toList commitSet)
 
 progDirPath = "./.nor"
@@ -48,48 +48,48 @@ getWorld = do
         Left err -> createProgDir >> (return initWorld)
         Right w -> return w
 
-getFile :: String -> IO(N.File)
+getFile :: String -> IO(File)
 getFile p = do
     contents <- readFile ("./"++p)
-    return $ N.File p (lines contents)
+    return $ File p (lines contents)
 
 commit :: World -> [String] -> IO (World)
 commit w@((_, os), hc) ("-a":names) = do
-    let Just files = sequence $ map (O.getObject os) (N.hashes hc)
-    let paths = map N.path files ++ names
+    let Just files = sequence $ map (O.getObject os) (hashes hc)
+    let paths = map path files ++ names
     commit w paths
 commit w@(core, hc) names = do
     fs <- mapM getFile names
-    let fhs = N.addHashableAs fs
-    let newCommitWithFiles = N.createCommit fhs (Just hc)
-    let (newHead,newCore) = State.runState (N.addCommit newCommitWithFiles) core
+    let fhs = addHashableAs fs
+    let newCommitWithFiles = createCommit fhs (Just hc)
+    let (newHead,newCore) = State.runState (addCommit newCommitWithFiles) core
     let w' = (newCore, newHead)
-    putStrLn $ show (N.cid newHead)
+    putStrLn $ show (cid newHead)
     return w'
 
 printCommits :: World -> IO ()
 printCommits ((commits, _) , hc) = do
-    putStrLn $ "HEAD: " ++ (show (N.cid hc))
+    putStrLn $ "HEAD: " ++ (show (cid hc))
     mapM (putStrLn.show) (Set.toList commits)
     return ()
 
 --check if file exists
-deleteFile (N.File p _) = do
+deleteFile (File p _) = do
     fileExists <- doesFileExist p
     when fileExists $ removeFile p
 
-deleteFiles :: [N.File] -> IO ()
+deleteFiles :: [File] -> IO ()
 deleteFiles fs = do 
     mapM deleteFile fs 
     return ()
 
 --create file if it doesnt exsit....
-restoreFile (N.File p cs) = do
+restoreFile (File p cs) = do
     handle <- openFile p WriteMode
     hPutStr handle $ unlines cs
     hClose handle
 
-restoreFiles :: [N.File] -> IO ()
+restoreFiles :: [File] -> IO ()
 restoreFiles fs = do 
     mapM restoreFile fs
     return ()
@@ -97,10 +97,10 @@ restoreFiles fs = do
 checkout :: World -> [String] -> IO (World)
 checkout w@((comSet, os), headCom) [hh] = do
     let h = O.hexToHash hh
-    let com = head $ Set.toList $ Set.filter ((h==).N.cid) comSet
-    let files = map (O.getObject os) (N.hashes com)
-    let Just dFiles = sequence $ map (O.getObject os) (N.hashes headCom)
-    let Just rFiles =  sequence $ map (O.getObject os) (N.hashes com)
+    let com = head $ Set.toList $ Set.filter ((h==).cid) comSet
+    let files = map (O.getObject os) (hashes com)
+    let Just dFiles = sequence $ map (O.getObject os) (hashes headCom)
+    let Just rFiles =  sequence $ map (O.getObject os) (hashes com)
     deleteFiles dFiles
     restoreFiles rFiles
     putStrLn $ "Updated repo to " ++ hh
@@ -109,13 +109,19 @@ checkout w@((comSet, os), headCom) [hh] = do
 files :: World -> [String] -> IO (World)
 files w@((comSet, os), headCom) [hh] = do
     let h = O.hexToHash hh
-    let com = head $ Set.toList $ Set.filter ((h==).N.cid) comSet
-    let Just files = sequence $ map (O.getObject os) (N.hashes com)
+    let com = head $ Set.toList $ Set.filter ((h==).cid) comSet
+    let Just files = sequence $ map (O.getObject os) (hashes com)
     putStrLn $ "Files for " ++ hh
     mapM (putStrLn.show) files
     return w 
 
-dispatch :: N.World -> String -> [String] -> IO (N.World)
+--rebase :: World -> [String] -> IO (World)
+--rebase w@((comSet,os), headCom) [hh] = do
+--   let upstreamHash = O.hexToHash hh
+--   let upstreamCom = head $ Set.toList $ Set.filter ((h==).cid) comSet
+
+
+dispatch :: World -> String -> [String] -> IO (World)
 -- Nor commands
 dispatch w "commit" ns = commit w ns
 dispatch w "tree" _ = printCommits w >> return w
