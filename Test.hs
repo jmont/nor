962328c573bfe4bf20abec8e32a7382ec7e83d2d
hashem <hnasarat@gmail.com>
2012-12-04 01:06:59 -0500

    WIP mkGood*

--- Test.hs
+++ Test.hs
@@ -6,6 +6,7 @@ import Test.QuickCheck.Arbitrary
 import Control.Applicative
 import Control.Monad
 import Data.List
+import Nor
 
 instance (Eq t, Arbitrary t) => Arbitrary (Edit t) where
     arbitrary = oneof [return C,
@@ -33,6 +34,37 @@ instance (Conflictable t, Arbitrary t) => Arbitrary (Conflict t) where
     arbitrary = do
         x <- arbitrary
         Conflict <$> return x <*> arbitrary `suchThat` (conflicts x)
+
+instance Arbitrary File where
+    arbitrary = do
+        len <- arbitrary
+        conts <- arbitrary `suchThat` ((< len) . length)
+        File <$> arbitrary <*> return conts
+
+mkGoodPatch :: Gen File -> Gen [Patch]
+mkGoodPatch gf = do
+    f <- gf
+    frequency
+        [ (1, return ( map (AP (path f)) [Change (ChangeHunk 0 (contents f) []),
+                                 RemoveEmptyFile])),
+          (9, do
+            chs <- (mkGoodCH gf 0)
+            return $ map ((AP (path f)) . Change) chs) ]
+
+
+mkGoodCH :: Gen File -> Int -> Gen [ChangeHunk]
+mkGoodCH gf startoff = do
+    f <- gf
+    if (startoff == length (contents f) - 1)
+    then return []
+    else do off <- choose (startoff, length . contents $ f)
+            endDellOff <- choose (off, (length (contents f)))
+            let dels = slice off endDellOff (contents f)
+            news <- arbitrary
+            liftM ((ChangeHunk off dels news) :) $ mkGoodCH gf (endDellOff + 1)
+
+    where slice  :: Int -> Int -> [a] -> [a]
+          slice from to xs = take (to - from + 1) (drop from xs)
 
 prop_getApplyEdits :: (Eq t, Arbitrary t, Show t) => [t] -> [t] -> Bool
 prop_getApplyEdits x y =
