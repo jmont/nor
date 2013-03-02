8626aded7bafa23f311d37219103a77b1644e359
Hashem Nasarat <hnasarat@gmail.com>
2013-03-02 14:15:42 -0500

    Make Commit polymorphic, update CoreExtender

--- Core.hs
+++ Core.hs
@@ -20,23 +20,22 @@ data CR a = CR { rc :: Core -> a }
 data CX a = CX { xc :: Core -> (a, Core) }
 
 -- list of all commits, hash->file, head commit, commitCount
-type Core = (Set.Set Commit, ObjectStore File)
+type Core = (Set.Set (Commit Hash), ObjectStore File)
 
 data File = File { path :: String -- Unix filepath: "/foo/bar/baz"
                  , contents :: [String] -- Simple representation for now
                  } deriving (Show,Eq)
 
-data Commit = Commit { parent :: Maybe Hash -- Initial commit has nothing
-                     , hashes :: [Hash] -- Hashes of all files at given time
-                     , cid :: Hash
-                     } deriving (Show)
+data Commit a = Commit { parent :: Maybe Hash -- Initial commit has nothing
+                       , cContents :: [a] -- Associated as with commit
+                       , cid :: Hash
+                       } deriving (Show)
 
 class Monad m => CoreReader m where
     readCore :: m Core
 
 class CoreReader m => CoreExtender m where
-    addFile   :: File   -> m Hash
-    addCommit' :: Commit -> m ()
+    addCommit' :: Commit File -> m (Commit Hash)
 
 instance Monad CR where
   return a = CR $ const a
@@ -53,21 +52,21 @@ instance CoreReader CX where
   readCore = CX $ \c -> (c, c)
 
 instance CoreExtender CX where
-  addFile file = CX $ \(cs, os) ->
-                            let (h, os') = addObject os file
-                            in (h, (cs, os'))
-  --(hash_of_new_file file core, new_core file core)
-  addCommit' c = CX $ \(cs, os) -> ((), (Set.insert c cs, os)) -- TODO fix
+  addCommit' c = CX $ \(cs, os) ->
+                        let fs = cContents c
+                            (hs, os') = addObjects os fs
+                            c' = Commit (parent c) hs (cid c)
+                        in (c', (Set.insert c' cs, os'))
 
 instance Serialize File where
     put (File p c) = put p >> put c
     get = File <$> get <*> get
 
-instance Ord Commit where
+instance Ord (Commit a) where
    compare c1 c2 = compare (cid c1) (cid c2)
-instance Eq Commit where
+instance Eq (Commit a) where
    c1 == c2 = cid c1 == cid c2
-instance Serialize Commit where
+instance Serialize a => Serialize (Commit a) where
     put (Commit pid hs id) = put pid >> put hs >> put id
     get = Commit <$> get <*> get <*> get
 
